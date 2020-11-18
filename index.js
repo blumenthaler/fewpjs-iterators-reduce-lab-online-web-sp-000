07054f684e932c8cee94447ce072db640de90f40
blumenthaler <blumenthaleralex@gmail.com>
2020-11-18 08:52:22 -0500

    commit tree 4e6bcfedb4f86de96cf41bd9d6fba5f946aa8abe

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((sum, battery) => sum += battery, 0)
+
+// Create a new variable called totalBatteries which is the sum of all of the battery amounts in the batteryBatches array. Naturally, use reduce() for this!
